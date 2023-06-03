7a404cb4a06b4cb0a42ee7e668d59843f7f27a91
Joao <zacarias1021@gmail.com>
2023-06-03 18:23:52 -0400

    "att and merge"

--- twitterProj1/twitter-front/js/models/Post.js
+++
@@ -1,8 +0,0 @@
-class Post {
-    constructor(id, conteudo, curtidas, userId) {
-        this.id = id;
-        this.conteudo = conteudo;
-        this.curtidas = curtidas;
-        this.userId = userId;
-    }
-}
